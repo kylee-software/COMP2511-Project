2863b92694ba7dcb06c64450628a6195ccff5833
Sam Thorley <samuel.thorley2000@gmail.com>
2021-07-12 14:14:18 +1000

    Added possible layout of integration test

---
+++ src/test/IntegrationTest.java
@@ -0,0 +1,46 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import unsw.loopmania.LoopManiaWorld;
+
+
+/**
+ * this class is a dummy class demonstrating how to setup tests for the project
+ * you should setup additional test classes in a similar fashion, aiming to achieve high coverage.
+ * A clickable "Run Test" link should appear if you have installed the Java Extension Pack properly.
+ */
+public class IntegrationTest {
+    @Test
+    public void blahTest(){
+        // Set gameMode
+        
+        // Create world
+
+        // Spawn slugs
+
+        // Spawn vampire castle
+
+        // place buildings
+
+        // move tick
+
+        // spawn items
+
+        // equip items
+
+        // check stats
+
+        // battle slug
+
+        // move to start
+
+        // sell and buy items
+
+        // win
+    }
+}
