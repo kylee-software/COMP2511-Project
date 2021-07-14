d0577bbf46dee837beeb2d99d2fcabfb13338805
Sam Thorley <samuel.thorley2000@gmail.com>
2021-07-14 12:57:59 +1000

    Added some basic functionality to battle

--- src/unsw/loopmania/Battle.java
+++ src/unsw/loopmania/Battle.java
@@ -2,27 +2,58 @@ package unsw.loopmania;
 
 import java.util.List;
 
+/**
+ * Class for a single battle in the backend world
+ */
 public class Battle {
 
     private Character character;
-    private TowerBuilding tower;
+    private List<TowerBuilding> towers;
     private List<AlliedSoldier> allies;
     private List<BasicEnemy> enemies;
 
-    public Battle(Character character, TowerBuilding tower, List<AlliedSoldier> allies, List<BasicEnemy> enemies) {
+    /**
+     * Constructor for a battle
+     * @param character
+     * @param towers - list of towers to join battle
+     * @param allies - list of allied soldiers to join battle
+     * @param enemies - list of enemies to join battle
+     */
+    public Battle(Character character, List<TowerBuilding> towers, List<AlliedSoldier> allies, List<BasicEnemy> enemies) {
         this.character = character;
-        this.tower = tower;
+        this.towers = towers;
         this.allies = allies;
         this.enemies = enemies;
     }
 
-    public void addEnemiesToTower() {
-        // TODO = need to implement this correctly and add javadoc
+    /**
+     * Given an enemy adds to the list of enemies for the battle
+     * @param enemy - enemy to add
+     */
+    public void addEnemyToBattle(BasicEnemy enemy) {
+        if (enemy.getHealth() > 0) {
+            this.enemies.add(enemy);
+        }
     }
 
+    /**
+     * Removes given enemy from list of enemies
+     * @param enemy - enemy to kill
+     */
+    public void killEnemy(BasicEnemy enemy) {
+        this.enemies.remove(enemy);
+    }
+
+    /**
+     * Checks if all enemies are dead
+     */
     public boolean areEnemiesDead() {
-        // TODO = need to implement this correctly and add javadoc
-        return false;
+        for (BasicEnemy enemy : enemies) {
+            if (enemy.getHealth > 0) {
+                return true;
+            }
+        }
+        return true;
     }
 
     public void sortListByCurrentHp() {
