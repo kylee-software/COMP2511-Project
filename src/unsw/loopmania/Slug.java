a7619f956ff0e734aa38564f2da138de4a9b8eac
Sam Thorley <samuel.thorley2000@gmail.com>
2021-07-15 12:56:45 +1000

    Added javadocs for slug

--- src/unsw/loopmania/Slug.java
+++ src/unsw/loopmania/Slug.java
@@ -2,6 +2,9 @@ package unsw.loopmania;
 
 import java.util.List;
 
+/**
+ * Basic enemy Slug in backend world
+ */
 public class Slug extends BasicEnemy {
 
     private static int expReward = 100;
@@ -9,12 +12,17 @@ public class Slug extends BasicEnemy {
     private List<Card> cardReward; // TODO:
     private static int battleRadius = 1;
     private static int supportRadius = 1;
-    private double speed; // TODO:
-
-    private AttackStrategy strategy;
+    private static double speed = 3; // Ticks per tile
+    private AttackStrategy strategy; // TODO:
     private static int damage = 7;
+    private static int health = 100;
 
-
+    /**
+     * Constructor for slug
+     * @param position - current position on map
+     * @param health
+     * @param type
+     */
     public Slug(PathPosition position, int health, String type) {
         super(position, health, type);
     }
@@ -24,26 +32,50 @@ public class Slug extends BasicEnemy {
         super.move();
     }
 
+    /**
+     * Getter for EXP reward when killed
+     * @return EXP reward
+     */
     public int getExpReward() {
         return expReward;
     }
 
+    /**
+     * Getter for gold reward when killed
+     * @return gold reward
+     */
     public int getGoldReward() {
         return goldReward;
     }
 
+    /**
+     * Getter for battle radius of slug
+     * @return battle radius (tiles)
+     */
     public int getBattleRadius() {
         return battleRadius;
     }
 
+    /**
+     * Getter for support radius of slug
+     * @return support radius (tiles)
+     */
     public int getSupportRadius() {
         return supportRadius;
     }
 
+    /**
+     * Getter for speed of slug
+     * @return support radius (ticks per tile)
+     */
     public double getSpeed() {
         return speed;
     }
 
+    /**
+     * Getter for slug damage
+     * @return damage
+     */
     public int getDamage() {
         return damage;
     }
