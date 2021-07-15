ba74e567d9216538b0edc1c7c39ecc38de627d24
Sam Thorley <samuel.thorley2000@gmail.com>
2021-07-15 12:50:44 +1000

    Added stats to slug

--- src/unsw/loopmania/Slug.java
+++ src/unsw/loopmania/Slug.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 public class Slug extends BasicEnemy {
 
-    private int expReward;
-    private int goldReward;
-    private List<Card> cardReward;
-    private int battleRadius;
-    private int supportRadius;
-    private double speed;
+    private static int expReward = 100;
+    private static int goldReward = 10;
+    private List<Card> cardReward; // TODO:
+    private static int battleRadius = 1;
+    private static int supportRadius = 1;
+    private double speed; // TODO:
 
     private AttackStrategy strategy;
-    private int damage;
+    private static int damage = 7;
 
 
     public Slug(PathPosition position, int health, String type) {
