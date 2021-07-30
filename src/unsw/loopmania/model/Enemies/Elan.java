59f646e9d5a064bbcf57f06e19b3b5f66bad6b97
Daniel <z5207355@login.cse.unsw.edu.au>
2021-07-31 08:48:19 +1000

    Creating Elan

---
+++ src/unsw/loopmania/model/Enemies/Elan.java
@@ -0,0 +1,68 @@
+package unsw.loopmania.model.Enemies;
+
+import unsw.loopmania.model.PathPosition;
+import unsw.loopmania.model.AttackStrategy.AttackStrategy;
+import unsw.loopmania.model.AttackStrategy.ElanAttack;
+
+public class Elan extends BasicEnemy {
+
+    private static int expReward = 500;
+    private static int goldReward = 50;
+    private static int battleRadius = 1;
+    private static int supportRadius = 1;
+    private static double speed = 1; // Ticks per tile
+    private static AttackStrategy strategy = new ElanAttack();
+    private static int damage = 30;
+    private static int health = 100;
+
+    /**
+     * Constructor for slug
+     * @param position - current position on map
+     */
+    public Elan(PathPosition position) {
+        super(position, health, damage, speed);
+    } 
+
+    @Override
+    public void move() {
+        super.move();
+    }   /**
+     * Getter for EXP reward when killed
+     * @return EXP reward
+     */
+    public int getExpReward() {
+        return expReward;
+    }
+
+    /**
+     * Getter for gold reward when killed
+     * @return gold reward
+     */
+    public int getGoldReward() {
+        return goldReward;
+    }
+
+    /**
+     * Getter for battle radius of slug
+     * @return battle radius (tiles)
+     */
+    public int getBattleRadius() {
+        return battleRadius;
+    }
+
+    /**
+     * Getter for support radius of slug
+     * @return support radius (tiles)
+     */
+    public int getSupportRadius() {
+        return supportRadius;
+    }
+
+    /**
+     * Getter for attack strategy
+     */
+    @Override
+    public AttackStrategy getAttackStrategy() {
+        return strategy;
+    }
+}
