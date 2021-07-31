f3dceccfca2e6fc264ee9baaaa2d11f1b42ca526
Daniel <z5207355@login.cse.unsw.edu.au>
2021-07-31 18:00:45 +1000

    changing class BasicEnemy to Enemy

--- src/test/BuildingsTests.java
+++ src/test/BuildingsTests.java
@@ -98,10 +98,10 @@ public class BuildingsTests {
         PathPosition position = new PathPosition(1, orderedPath);
         TrapBuilding trapBuilding = new TrapBuilding(position);
         
-        BasicEnemy slug = new Slug(position);
+        Enemy slug = new Slug(position);
         assertEquals(trapBuilding.damageEnemy(slug), 0);
 
-        BasicEnemy vampire = new Vampire(position);
+        Enemy vampire = new Vampire(position);
         assertEquals(trapBuilding.damageEnemy(vampire), 40);
         assert(!trapBuilding.shouldExist().get());
     }
@@ -116,7 +116,7 @@ public class BuildingsTests {
         TrapBuilding trapBuilding = new TrapBuilding(new SimpleIntegerProperty(1), new SimpleIntegerProperty(1));
         trapBuilding.setPathPosition(position);
         assertEquals(trapBuilding.getPathPosition(), position);
-        BasicEnemy slug = new Slug(position);
+        Enemy slug = new Slug(position);
     }
 
     @Test
